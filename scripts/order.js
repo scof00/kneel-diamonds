671a48f8d1199fbdb455e6fdbc6693e4d7186861
scof00 <scofprofessional@gmail.com>
2024-05-07 14:35:55 -0400

    Finished

--- scripts/order.js
+++ scripts/order.js
@@ -12,16 +12,17 @@ export const PlaceOrder = () => {
 }
 
 export const Orders = async () => {
-    const fetchResponse = await fetch("http://localhost:8088/orders")
+    const fetchResponse = await fetch("http://localhost:8088/orders?_expand=metal&_expand=style&_expand=size")
     const orders = await fetchResponse.json()
     let ordersHTML =""
     let orderCounter = 0
+    
     let divStringArray = orders.map(
         (order) => {
-            
+            const orderPrice = order.metal.price + order.style.price + order.size.price
             orderCounter ++
             return `<div>
-            Order #${orderCounter}
+            Order #${orderCounter} cost $${orderPrice}
             </div>`
         }
     )
